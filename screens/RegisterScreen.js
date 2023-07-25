210eb6cf97ab4e77439a32fbe780c3fe3dd97cfe
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-07-25 20:16:20 +0300

    fixed register and login

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useMutation } from "@tanstack/react-query";
 import {
   View,
